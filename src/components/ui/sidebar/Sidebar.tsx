6df0421c745ba4f0ba3375e48f491cbe3bae720c
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-02-17 17:46:15 -0500

    Agrega dependencia de zustand y enlaces en la barra lateral

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-import { IoCloseOutline, IoSearchOutline } from "react-icons/io5";
+import Link from "next/link";
+import { IoCloseOutline, IoLogInOutline, IoLogOutOutline, IoPeopleOutline, IoPersonOutline, IoSearchOutline, IoShirtOutline, IoTicketOutline } from "react-icons/io5";
 
 export const Sidebar = () => {
+
+ 
   return (
     <div>
       <div className="fixed top-0 left-0 w-dvw h-screen z-10 bg-black opacity-30"></div>
@@ -27,6 +30,65 @@ export const Sidebar = () => {
             placeholder="Buscar"
           />
         </div>
+
+        <Link
+          href="/"
+          className="flex items-center mt-8 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+            <IoPersonOutline size={30} />
+            <span className="ml-3">Perfil</span>
+        </Link>
+
+        <Link
+          href="/orders"
+          className="flex items-center mt-8 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+            <IoTicketOutline size={30} />
+            <span className="ml-3">Ordenes</span>
+        </Link>
+
+        <Link
+          href="/login"
+          className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+            <IoLogInOutline size={30} />
+            <span className="ml-3">Ingresar</span>
+        </Link>
+
+        <Link
+          href="/logout"
+          className="flex items-center mt-8 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+            <IoLogOutOutline size={30} />
+            <span className="ml-3">Salir</span>
+        </Link>
+
+        <div className="w-full h-px bg-gray-200 my-8"></div>
+
+        <Link
+          href="/products"
+          className="flex items-center mt-8 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+            <IoShirtOutline size={30} />
+            <span className="ml-3">Productos</span>
+        </Link>
+
+        <Link
+          href="/orders"
+          className="flex items-center mt-8 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+            <IoTicketOutline size={30} />
+            <span className="ml-3">Ordenes</span>
+        </Link>
+
+        <Link
+          href="/users"
+          className="flex items-center mt-8 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+            <IoPeopleOutline size={30} />
+            <span className="ml-3">Usuarios</span>
+        </Link>
+
       </nav>
     </div>
   );
