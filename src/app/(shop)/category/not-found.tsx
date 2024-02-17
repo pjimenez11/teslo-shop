ba166b1ebfe4f23191442c2bb66d7c9cbddabc39
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-02-16 20:19:40 -0500

    Agrega dependencia de react-icons y realiza cambios en la página de categoría y en el menú superior

---
+++ src/app/(shop)/category/not-found.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function NotFountPage() {
+  return (
+    <div>
+      <h1>404 Not found</h1>
+      <Link href="/">Regresar</Link>
+    </div>
+  );
+}
--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -1,6 +1,7 @@
 import { titleFont } from "@/config/fonts";
 import Link from "next/link";
 import React from "react";
+import { IoCartOutline, IoSearchOutline } from "react-icons/io5";
 
 export const TopMenu = () => {
   return (
@@ -10,7 +11,46 @@ export const TopMenu = () => {
           <span className={`${titleFont.className} antialiased font-bold`}>
             Teslo
           </span>
+          <span> | Shop</span>
         </Link>
+      </div>
+
+      <div className="hidden sm:block">
+        <Link
+          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          href="/category/men"
+        >
+          Hombres
+        </Link>
+        <Link
+          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          href="/category/women"
+        >
+          Mujeres
+        </Link>
+        <Link
+          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          href="/category/kids"
+        >
+          Niños
+        </Link>
+      </div>
+
+      <div className="flex items-center">
+        <Link href="/search" className="px-2">
+          <IoSearchOutline className="w-5 h-5" />
+        </Link>
+        <Link href="/cart" className="px-2">
+          <div className="relative">
+            <span className="absolute px-1 text-xs rounded-full font-bold -top-2 -right-2 bg-blue-700 text-white">
+              3
+            </span>
+            <IoCartOutline className="w-5 h-5" />
+          </div>
+        </Link>
+        <button className="m-2 p-2 rounded-md transition-all hover:bg-gray-100">
+          Menú
+        </button>
       </div>
     </nav>
   );
