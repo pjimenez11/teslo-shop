e30113a7fa81e87a8a64f38913718171cd378228
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-02-17 22:35:19 -0500

    Agrega componente SizeSelector y realiza cambios en la página de producto

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,5 @@ export * from "./ui/sidebar/Sidebar";
 
 export * from "./products/product-grid/ProductGrid";
 export * from "./products/product-grid/ProductGridItem";
+
+export * from "./product/size-selector/SizeSelector";
