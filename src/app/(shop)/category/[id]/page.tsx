5d128581883a131cb9ea016e33886fe6078bcced
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-02-17 21:53:33 -0500

    Añadir página de categoría y menú superior para niños

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,21 +1,47 @@
+import { ProductGrid, Title } from "@/components";
 import { notFound } from "next/navigation";
+import { initialData } from "../../../../seed/seed";
+import { Category } from "@/interfaces";
 
 interface Props {
   params: {
-    id: string;
+    id: Category;
   };
 }
+
+const products = initialData.products;
 
 export default function CategoryPage({ params }: Props) {
   const { id } = params;
 
-  if (id === "kids") {
-    notFound();
+  const labels: Record<Category, string> = {
+    men: "para hombres",
+    women: "para mujeres",
+    kid: "para niños",
+    unisex: "para todos",
+  };
+
+  const subTitle : Record<Category, string> = {
+    men: "Ropa para hombres",
+    women: "Ropa para mujeres",
+    kid: "Ropa para niños",
+    unisex: "Ropa para todos",
+  };
+
+  if (!Object.keys(labels).includes(id)) {
+    return notFound();
   }
 
+  const filteredProducts = products.filter((product) => product.gender === id);
+
   return (
-    <div>
-      <h1>Categori Page {id}</h1>
-    </div>
+    <>
+      <Title
+        title={`Artículos ${labels[id]}`}
+        subtitle={subTitle[id]}
+        className="m-2"
+      />
+      <ProductGrid products={filteredProducts} />
+    </>
   );
 }
