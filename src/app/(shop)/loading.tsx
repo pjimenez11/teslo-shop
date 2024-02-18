d90b3f75df426c1a707129019a9a96b0d9694014
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-02-18 11:35:00 -0500

    Actualización de traducciones en la página de carga y en el carrusel de productos

--- src/app/(shop)/loading.tsx
+++ src/app/(shop)/loading.tsx
@@ -3,7 +3,7 @@ export default function LoadinPage() {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="animate-spin h-5 w-5 mr-3 bg-gray-600"></div>
-      <span className="">Loading...</span>
+      <span className="">Cargando...</span>
     </div>
   );
 }
--- src/components/product/slideshow/ProductSlideshow.tsx
+++ src/components/product/slideshow/ProductSlideshow.tsx
@@ -1,7 +1,17 @@
 "use client";
+import { useState } from "react";
 
+import { Swiper as SwiperObject } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
+
 import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+
+import "./slideshow.css";
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+import Image from "next/image";
 
 interface Props {
   images: string[];
@@ -10,18 +20,34 @@ interface Props {
 }
 
 export const ProductSlideshow = ({ images, title, className }: Props) => {
+  const [thumbsSwiper, setThumbsSwiper] = useState<SwiperObject>();
+
   return (
     <div className={`${className} `}>
       <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
+        style={
+          {
+            "--swiper-navigation-color": "#fff",
+            "--swiper-pagination-color": "#fff",
+          } as React.CSSProperties
+        }
+        spaceBetween={10}
+        navigation={true}
+        thumbs={{ swiper: thumbsSwiper }}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper2"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <Image
+              src={`/products/${image}`}
+              alt={title}
+              width={1024}
+              height={800}
+              className="rounded-lg"
+            />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
