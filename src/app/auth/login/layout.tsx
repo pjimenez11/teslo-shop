5694c8c684c7863f9732fc242dec038ec0cc7246
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-02-19 21:35:02 -0500

    Actualizar diseño de la página de inicio de sesión

--- src/app/auth/login/layout.tsx
+++ src/app/auth/login/layout.tsx
@@ -5,8 +5,8 @@ export default function ShopLayout({
  children: React.ReactNode;
 }) {
   return (
-    <main className="min-h-dvh bg-gray-500">
+    <>
       {children}
-    </main>
+    </>
   );
 }
