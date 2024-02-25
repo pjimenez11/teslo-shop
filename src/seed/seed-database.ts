9480a5be76a9edd43352cd2b45a2051e0ded5077
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-02-25 14:33:23 -0500

    Agregar datos iniciales a la base de datos

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -2,7 +2,6 @@ import { initialData } from "./seed";
 import prisma from "../lib/prisma";
 
 async function main() {
-
   //1. Borrar todos los datos de la base de datos
   await Promise.all([
     prisma.productImage.deleteMany(),
@@ -10,8 +9,12 @@ async function main() {
     prisma.category.deleteMany(),
   ]);
 
+  const { categories, products } = initialData;
+
   //2. Insertar datos iniciales
-  
+  const categoriesData = categories.map((categorie) => ({
+    name: categorie,
+  }));
 
   console.log("Seed ejecutado correctamente");
 }
