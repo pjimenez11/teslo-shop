582f2beb718abbe933c5a93375f9a0cae5efa73b
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-02-25 14:22:26 -0500

    Borrar y insertar datos iniciales en la base de datos

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,7 +1,18 @@
 import { initialData } from "./seed";
+import prisma from "../lib/prisma";
 
 async function main() {
-  console.log(initialData);
+
+  //1. Borrar todos los datos de la base de datos
+  await Promise.all([
+    prisma.productImage.deleteMany(),
+    prisma.product.deleteMany(),
+    prisma.category.deleteMany(),
+  ]);
+
+  //2. Insertar datos iniciales
+  
+
   console.log("Seed ejecutado correctamente");
 }
 
