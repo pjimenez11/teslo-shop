facee0d1d938a8aebfa642bb4f854b385d6d4114
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-02-28 12:16:55 -0500

    Agrega importación y llamada a la función getPaginatedProductsWithImages en ShopPage

--- src/actions/products/product-pagination.ts
+++ src/actions/products/product-pagination.ts
@@ -1,5 +1,5 @@
+"use server";
 import prisma from "@/lib/prisma";
-("use server");
 
 export const getPaginatedProductsWithImages = async () => {
   try {
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,13 @@
+import { getPaginatedProductsWithImages } from "@/actions";
 import { ProductGrid, Title } from "@/components";
 import { initialData } from "@/seed/seed";
 
 const products = initialData.products;
 
-export default function ShopPage() {
+export default async function ShopPage() {
+
+  const productTemp = await getPaginatedProductsWithImages();
+
   return (
     <>
       <Title title="Tienda" subtitle="Todos los productos" className="m-2" />
