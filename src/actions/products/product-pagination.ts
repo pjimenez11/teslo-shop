3c6185e30d2bb8a6d70bd0a997cfdf7d7d01bbf5
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-02-28 13:03:29 -0500

    Actualizar paginación de productos y corregir errores en la interfaz de productos

--- src/actions/products/product-pagination.ts
+++ src/actions/products/product-pagination.ts
@@ -1,9 +1,21 @@
 "use server";
 import prisma from "@/lib/prisma";
 
-export const getPaginatedProductsWithImages = async () => {
+interface PaginationOptions {
+  page?: number;
+  take?: number;
+}
+
+export const getPaginatedProductsWithImages = async ({
+  page = 1,
+  take = 12,
+}: PaginationOptions) => {
+  if (isNaN(Number(page))) page = 1;
+  if (page < 1) page = 1;
   try {
     const products = await prisma.product.findMany({
+      take: take,
+      skip: (page - 1) * take,
       include: {
         ProductImage: {
           take: 2,
@@ -13,6 +25,20 @@ export const getPaginatedProductsWithImages = async () => {
         },
       },
     });
-    console.log(products);
-  } catch (error) {}
+
+    const totalCount = await prisma.product.count({});
+
+    const totalPages = Math.ceil(totalCount / take);
+
+    return {
+      currentPage: page,
+      totalPages: totalPages,
+      products: products.map((product) => ({
+        ...product,
+        images: product.ProductImage.map((image) => image.url),
+      })),
+    };
+  } catch (error) {
+    throw new Error("Error, no se pudieron obtener los productos con imagenes");
+  }
 };
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,13 +1,23 @@
 import { getPaginatedProductsWithImages } from "@/actions";
 import { ProductGrid, Title } from "@/components";
-import { initialData } from "@/seed/seed";
+import { redirect } from "next/navigation";
 
-const products = initialData.products;
+interface Props {
+  searchParams: {
+    page?: string;
+  };
+}
 
-export default async function ShopPage() {
+export default async function ShopPage({ searchParams }: Props) {
+  const page = searchParams.page ? parseInt(searchParams.page) : 1;
+  const { products, currentPage, totalPages } =
+    await getPaginatedProductsWithImages({ page });
 
-  const productTemp = await getPaginatedProductsWithImages();
-
+  console.log("currentPage", currentPage);
+  console.log("totalPages", totalPages);
+  if (products.length === 0) {
+    redirect("/");
+  }
   return (
     <>
       <Title title="Tienda" subtitle="Todos los productos" className="m-2" />
