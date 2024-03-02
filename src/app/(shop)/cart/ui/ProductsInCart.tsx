26b8beba2f51bd57505e12ab5afd7fbabf6d12e4
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-03-01 20:16:18 -0500

    Agregado componente de carga para productos en el carrito

--- src/app/(shop)/cart/ui/ProductsInCart.tsx
+++ src/app/(shop)/cart/ui/ProductsInCart.tsx
@@ -4,31 +4,22 @@ import { useCartStore } from "@/store";
 import { redirect } from "next/navigation";
 import { QuantitySelector } from "../../../../components/product/quantity-selector/QuantitySelector";
 import { useEffect, useState } from "react";
+import { LoandingCartProduct } from "@/components";
 
 export const ProductsInCart = () => {
   const [loaded, setLoaded] = useState(false);
   const productsInCart = useCartStore((state) => state.cart);
 
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
   if (!loaded)
     return (
-      <div className="flex mb-5 w-full p-2 rounded bg-gray-200 animate-pulse">
-        <div
-          className="mr-5 rounded animate-pulse bg-gray-400"
-          style={{
-            width: "100px",
-            height: "100px",
-          }}
-        />
-
-        <div className="w-full">
-          <p className="animate-pulse bg-gray-400 w-full rounded mb-2">
-            &nbsp;
-          </p>
-          <p className="animate-pulse bg-gray-400 w-1/2 rounded mb-2">&nbsp;</p>
-          <button className="mt-3 animate-pulse bg-gray-400  w-full rounded">
-            &nbsp;
-          </button>
-        </div>
+      <div>
+        <LoandingCartProduct />
+        <LoandingCartProduct />
+        <LoandingCartProduct />
       </div>
     );
 
