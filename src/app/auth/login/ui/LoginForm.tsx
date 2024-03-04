ebeb9b1bdecd6ced6e04480fbfeb8426413807ec
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-03-04 12:28:41 -0500

    Actualizar página de inicio de sesión

---
+++ src/app/auth/login/ui/LoginForm.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Link from "next/link";
+
+export const LoginForm = () => {
+  return (
+    <div className="flex flex-col">
+      <label htmlFor="email">Correo electrónico</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="email"
+      />
+
+      <label htmlFor="email">Contraseña</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="email"
+      />
+
+      <button className="btn-primary">Ingresar</button>
+
+      {/* divisor l ine */}
+      <div className="flex items-center my-5">
+        <div className="flex-1 border-t border-gray-500"></div>
+        <div className="px-2 text-gray-800">O</div>
+        <div className="flex-1 border-t border-gray-500"></div>
+      </div>
+
+      <Link href="/auth/new-account" className="btn-secondary text-center">
+        Crear una nueva cuenta
+      </Link>
+    </div>
+  );
+};
