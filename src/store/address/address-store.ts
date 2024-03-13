76fb7e96f59e41ede36f0d9eac3dc25a2db2b698
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-03-12 21:54:11 -0500

    Actualización de la función de búsqueda para incluir filtros de fecha y categoría.

---
+++ src/store/address/address-store.ts
@@ -0,0 +1,41 @@
+import { set } from "zod";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface State {
+  address: {
+    firstName: string;
+    lastName: string;
+    address?: string;
+    address2?: string;
+    city: string;
+    postalCode: string;
+    country: string;
+    phone: string;
+  };
+  setAddress: (address: State["address"]) => void;
+}
+
+export const useAddressStore = create<State>()(
+  persist(
+    (set, get) => ({
+      address: {
+        firstName: "",
+        lastName: "",
+        address: "",
+        address2: "",
+        city: "",
+        postalCode: "",
+        country: "",
+        phone: "",
+      },
+      setAddress: (address) => {
+        set({ address });
+      },
+    }),
+
+    {
+      name: "address-storage",
+    }
+  )
+);
