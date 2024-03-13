f7f51c0c0412cd4957ceb6b2def0751691fcc2e8
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-03-12 22:44:11 -0500

    Agregada acción para establecer la dirección del usuario

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,6 +2,8 @@ export * from "./auth/login";
 export * from "./auth/logout";
 export * from "./auth/register";
 
+export * from "./address/set-user-address";
+
 export * from "./country/get-countries";
 
 export * from "./products/product-pagination";
