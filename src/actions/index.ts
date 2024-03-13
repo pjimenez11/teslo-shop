e4feebea11dd770150e13b62f31c222a74820642
pjimenez11 <pjimenez1025@uta.edu.ec>
2024-03-12 21:36:38 -0500

    Agregados archivos de países y actualizada interfaz de productos

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,6 +2,8 @@ export * from "./auth/login";
 export * from "./auth/logout";
 export * from "./auth/register";
 
+export * from "./country/get-countries";
+
 export * from "./products/product-pagination";
 export * from "./products/get-product-by-slug";
 export * from "./products/get-stock-by-slug";
